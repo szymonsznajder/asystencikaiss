a7a2e0b9348c892f46a009f32400765e3873a105
szymonsznajer <sznajder.szymon@gmail.com>
2024-11-16 21:12:32 +0000

    dwqdwqe

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,4 @@
-/* eslint-disable operator-linebreak */
-/* eslint-disable import/no-unresolved */
-/* eslint-disable import/no-absolute-path */
 import {
-  sampleRUM,
   buildBlock,
   loadHeader,
   loadFooter,
@@ -11,50 +7,11 @@ import {
   decorateSections,
   decorateBlocks,
   decorateTemplateAndTheme,
-  waitForLCP,
-  loadBlocks,
-  toClassName,
-  getMetadata,
-  loadScript,
-  toCamelCase,
+  waitForFirstImage,
+  loadSection,
+  loadSections,
   loadCSS,
-} from '/scripts/aem.js';
-
-import { } from '/plusplus/src/siteConfig.js';
-
-const LCP_BLOCKS = []; // add your LCP blocks to the lis
-const AUDIENCES = {
-  mobile: () => window.innerWidth < 600,
-  desktop: () => window.innerWidth >= 600,
-  // define your custom audiences here as needed
-};
-
-/**
-     * Gets all the metadata elements that are in the given scope.
-     * @param {String} scope The scope/prefix for the metadata
-     * @returns an array of HTMLElement nodes that match the given scope
-     */
-export function getAllMetadata(scope) {
-  return [...document.head.querySelectorAll(`meta[property^="${scope}:"],meta[name^="${scope}-"]`)]
-    .reduce((res, meta) => {
-      const id = toClassName(meta.name
-        ? meta.name.substring(scope.length + 1)
-        : meta.getAttribute('property').split(':')[1]);
-      res[id] = meta.getAttribute('content');
-      return res;
-    }, {});
-}
-
-// Define an execution context
-const pluginContext = {
-  getAllMetadata,
-  getMetadata,
-  loadCSS,
-  loadScript,
-  sampleRUM,
-  toCamelCase,
-  toClassName,
-};
+} from './aem.js';
 
 /**
  * Builds hero block and prepends to main in a new section.
@@ -81,19 +38,6 @@ async function loadFonts() {
   } catch (e) {
     // do nothing
   }
-}
-// added for modal handling, see adobe docs
-// eslint-disable-next-line no-unused-vars
-function autolinkModals(element) {
-  element.addEventListener('click', async (e) => {
-    const origin = e.target.closest('a');
-
-    if (origin && origin.href && origin.href.includes('/modals/')) {
-      e.preventDefault();
-      const { openModal } = await import(`${window.hlx.codeBasePath}/blocks/modal/modal.js`);
-      openModal(origin.href);
-    }
-  });
 }
 
 /**
@@ -128,23 +72,13 @@ export function decorateMain(main) {
  * @param {Element} doc The container element
  */
 async function loadEager(doc) {
-  window.cmsplus.debug('loadEager');
   document.documentElement.lang = 'en';
-  // Add below snippet early in the eager phase
-  if (getMetadata('experiment') ||
-    Object.keys(getAllMetadata('campaign')).length ||
-    Object.keys(getAllMetadata('audience')).length) {
-    // eslint-disable-next-line import/no-relative-packages
-    const { loadEager: runEager } = await import('../plusplus/plugins/experimentation/src/index.js');
-    await runEager(document, { audiences: AUDIENCES }, pluginContext);
-  }
-
   decorateTemplateAndTheme();
   const main = doc.querySelector('main');
   if (main) {
     decorateMain(main);
     document.body.classList.add('appear');
-    await waitForLCP(LCP_BLOCKS);
+    await loadSection(main.querySelector('.section'), waitForFirstImage);
   }
 
   try {
@@ -162,31 +96,18 @@ async function loadEager(doc) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
-  window.cmsplus.debug('loadLazy');
   const main = doc.querySelector('main');
-  await loadBlocks(main);
-  autolinkModals(doc); // added for modal handling, see adobe docs
+  await loadSections(main);
+
   const { hash } = window.location;
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
-  if (!window.hlx.suppressFrame) { // added for sidekick library - see block party
-    loadHeader(doc.querySelector('header'));
-    loadFooter(doc.querySelector('footer'));
-  }
+
+  loadHeader(doc.querySelector('header'));
+  loadFooter(doc.querySelector('footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   loadFonts();
-  if ((getMetadata('experiment') ||
-    Object.keys(getAllMetadata('campaign')).length ||
-    Object.keys(getAllMetadata('audience')).length)) {
-    // eslint-disable-next-line import/no-relative-packages
-    const { loadLazy: runLazy } = await import('/plusplus/plugins/experimentation/src/index.js');
-    await runLazy(document, { audiences: AUDIENCES }, pluginContext);
-  }
-
-  sampleRUM('lazy');
-  sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
-  sampleRUM.observe(main.querySelectorAll('picture > img'));
 }
 
 /**
@@ -194,21 +115,12 @@ async function loadLazy(doc) {
  * without impacting the user experience.
  */
 function loadDelayed() {
-  window.cmsplus.debug('loadDelayed timer start');
   // eslint-disable-next-line import/no-cycle
   window.setTimeout(() => import('./delayed.js'), 3000);
   // load anything that can be postponed to the latest here
 }
 
 async function loadPage() {
-  window.cmsplus.debug('loadPage');
-  const urlParams = new URLSearchParams(window.location.search);
-  // added for sidekick library - see block party
-  if (urlParams.get('suppressFrame') || window.location.pathname.includes('tools/sidekick')) {
-    window.hlx.suppressFrame = true;
-    document.body.querySelector('header').remove();
-    document.body.querySelector('footer').remove();
-  }
   await loadEager(document);
   await loadLazy(document);
   loadDelayed();
